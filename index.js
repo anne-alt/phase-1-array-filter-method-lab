fa676617ea2a7c4bcf802189ce4548acf1aad7f2
Anne Ogongo <anneogongo98@gmail.com>
2022-08-21 21:05:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, factor) {
+    return drivers.filter(function(driver) {
+        return(driver.toUpperCase() === factor.toUpperCase());
+    })
+}
+
+function fuzzyMatch(drivers, factor) {
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase().substring(0, factor.length) === factor.toLowerCase()
+    })
+}
+
+function matchName(drivers, factor) {
+    return drivers.filter(function(driver) {
+        return driver.name === factor;
+    })
+}
